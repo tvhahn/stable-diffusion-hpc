66d6b35f9eea5957a0a172099a6fd81e739abb10
tvhahn <tvhahn@gmail.com>
2022-08-23 20:22:12 -0700

    update

--- install_env_hpc.sh
+++ install_env_hpc.sh
@@ -3,8 +3,8 @@ module load python/3.8
 virtualenv ~/ldm
 source ~/ldm/bin/activate
 pip install --no-index --upgrade pip
-pip install --no-index torch torchvision numpy==1.19.2 jupyterlab
-pip install --no-index -r requirements.txt
+pip install --no-index torch torchvision numpy jupyterlab
+pip install -r requirements.txt
 
 
 # create bash script for opening jupyter notebooks https://stackoverflow.com/a/4879146/9214620
